e0c91d1837b8d7ee2c978319b12787b620662279
rwa4shivamum <rwa4.shivam.um@gmail.com>
2026-06-29 14:23:03 +0530

    first commit

---
+++ index.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "HEllo world" << endl;
+    // string num = "shicam";
+    return 0;
+}
---
+++ index.py
@@ -0,0 +1,77 @@
+# print("hello world")
+# name = input("Enter your name:")
+# print(name)
+# print(100)
+# print(50 + 20)
+
+# name = "Shivam"
+# age = 22
+
+# print(name, age)
+
+# # New Line
+# print("Hello\nStudents")
+
+# # tab space
+# print("Python\tJava\tC++")
+
+# # sep
+# print("2026", "05", "12", sep="-")
+
+# # end
+# print("Hello", end=" ")
+# print("Students")
+
+# name = input("Enter your name:")
+# print(name)
+# print(type(name))
+
+# age = input("Enter your age:")
+# print(age)
+# print(type(age))
+
+# num1 = int(input("Enter num1:"))
+# num2 = int(input("Enter num2:"))
+# print(num1 + num2)
+# print(type(num1))
+
+# num1 = int("2")
+# # num2 = int(num1)
+# num2 = int("3")
+# print(num1 + num2)
+
+# age = 22
+# name = "shivam"
+# print(age, name)
+# age = 24
+# name = "sani"
+# print(age,name)
+
+# fullNameAndThen
+
+# print = 22
+# print(print)
+# a = "shvam"
+# b = 22
+# c = 2024
+# a,b,c = "shivam", 20, 2024
+# print(a,b,c)
+# x = y = z = 100
+# print(x, y, z)
+
+
+#Practice Quesiton
+# Create variable for your:
+# - Name
+# - Age
+# - City
+# - College Name
+
+
+
+name = "shivam"
+age = 22
+salary = 22.22
+isAdult = True
+
+print(type(name), type(age), type(salary), type(isAdult))
