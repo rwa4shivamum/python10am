043c875345879d7e14f2c5003b7e9a3eefb4ed53
rwa4shivamum <rwa4.shivam.um@gmail.com>
2026-07-02 10:32:40 +0530

    loops

---
+++ Loops.py
@@ -0,0 +1,50 @@
+# if(true){
+#     print("hello")
+# }
+if True:
+    print("hello")
+
+# name = "shivam"
+# name = "aani"
+# name = "jsbf"
+# name = ["shivam", "aani","jsbf"]
+# DRY(don't repeat yourself)
+# i = "shivam"
+# name = "sjhivam"
+
+# for name in range(5):
+#     if True:
+#       print("hello",name)
+
+# name = "python"
+# for ch in name:
+#     print(ch)
+
+#range(start, stop)
+# for i in range(5,10):
+#     print(i)
+
+#range(start, stop, step)
+# for i in range(5,20,2):
+#     print(i)  
+
+
+#loop flow of execution
+#step 1: initialization Or declaration i=0
+#step 2: comparison i <= 5;
+#step 3: execution 
+#stpe 4: increment/decrement 
+
+# i=1
+# while i<=20:
+#     print(i)
+#     if(i%2==0):
+#         i += 3
+#     else:
+#         i += 1
+
+#1,2,5,6,7,10,13,14
+
+
+#nested Loop
+
