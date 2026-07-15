bbd28c732b816642609d867c4cda80ec5719ee7f
rwa4shivamum <rwa4.shivam.um@gmail.com>
2026-07-15 11:55:57 +0530

    listTuple

---
+++ collectionofData/list/collectionoflist.py
@@ -0,0 +1,34 @@
+# name1="kjsbfd"
+# name2="ajkbsd"
+# name3="kjsbd"
+# name4="sdfsdf"
+
+
+# print(collectionList)
+# #1.allows duplicate 
+# #2.mutable(can cahnge the value)
+# #3.value get by indexed or change by indexed
+# #4.supported ordered manner
+# collectionList[1] = "sani"
+# print(collectionList)
+# # print(collectionList[0])
+
+# print(len(collectionList)) #len(collectionList)
+# # for i in collectionList:
+# #     print(i)
+
+
+# #list                0         1         2         3         4
+# collectionList = ["shivam", "shivam", "ahajds", "kjbsd", "skdjfns"]
+# collectionList[1] = "sani"
+# print(collectionList[1])
+# # for i in collectionList:
+# #     print(i)
+# print(len(collectionList))
+# lengthOfCollect = len(collectionList)
+# for i in range(lengthOfCollect):
+#     if(i%2==0):
+#         collectionList[i] = "some"
+#     print(collectionList[i])
+
+
---
+++ collectionofData/tuple/collectionofTuple.py
@@ -0,0 +1,9 @@
+#tuple is used to store multiple value 
+#tuple values i immutable(cannot be change)
+#duplicates allows
+#order
+#here () circle braces used
+
+tupleGrid = (12,12,13,14,15,16)
+# tupleGrid(0) = 14 cannot be change
+print(tupleGrid)
